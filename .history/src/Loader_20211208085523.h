0923ca23066ba9a98f089dc8f45b9c83c5a2265e
christezeng <christezeng@gmail.com>
2021-12-08 09:01:18 +0800

    MipMap实现

--- .history/src/Loader_20211208085523.h
+++ .history/src/Loader_20211208085523.h
@@ -83,7 +83,6 @@ public:
 
 			glGenerateMipmap(GL_TEXTURE_2D);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -0.4f);
 		}
 		else
 		{
--- .history/src/Loader_20211208085711.h
+++ .history/src/Loader_20211208085711.h
@@ -83,7 +83,7 @@ public:
 
 			glGenerateMipmap(GL_TEXTURE_2D);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -0.4f);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -1);
 		}
 		else
 		{
